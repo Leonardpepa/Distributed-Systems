83ad72f0edacd8053f0b42203e279bd83ff29eee
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-10-18 21:13:55 +0300

    make account serializable

--- Server_ATM/src/Model/Account.java
+++ Server_ATM/src/Model/Account.java
@@ -1,6 +1,8 @@
 package Model;
 
-public class Account {
+import java.io.Serializable;
+
+public class Account implements Serializable {
     private int id;
     private int pin;
     private String name;
