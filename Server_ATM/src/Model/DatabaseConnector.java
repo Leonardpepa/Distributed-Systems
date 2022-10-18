5d67979f0ecdb44d2b46a02912c32cac1466f3a1
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-10-19 00:51:31 +0300

    create db and table if not exist

--- Server_ATM/src/Model/DatabaseConnector.java
+++ Server_ATM/src/Model/DatabaseConnector.java
@@ -3,6 +3,7 @@ package Model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnector {
     private final Connection dbConnection;
@@ -11,6 +12,20 @@ public class DatabaseConnector {
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             this.dbConnection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/" + databaseName + "?user=root&password=");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void initDB(String name){
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection dbConnection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/?user=root&password=");
+            Statement statement = dbConnection.createStatement();
+            statement.executeQuery("CREATE DATABASE IF NOT EXISTS " + name);
+            statement.executeQuery("CREATE TABLE IF NOT EXISTS "+ name +".`account` (`id` INT NOT NULL , `pin` INT NOT NULL , `name` VARCHAR(255) NOT NULL , `balance` DOUBLE NOT NULL );");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
