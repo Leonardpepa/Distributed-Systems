42da1bbf72670e8a91d410c72b569ca500fd7c7f
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2022-11-18 16:59:56 +0200

    INTELIJJ working

--- lab2/Server_ATM/src/Main.java
+++ lab2/Server_ATM/src/Main.java
@@ -1,16 +1,5 @@
-import Controller.Server;
-import Model.DatabaseConnector;
-
-import java.rmi.RemoteException;
-
 public class Main {
     public static void main(String[] args) {
-        DatabaseConnector.initDB("bank", 0);
-        try {
-            Server server = new Server();
-
-        } catch (RemoteException e) {
-            throw new RuntimeException(e);
-        }
+        
     }
 }
