7f88c1e3a39f56bf92fa23d0777204c44c4f8563
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-11-25 04:10:36 +0200

    Pyro: added comments

--- lab3/Pyro/Pyro_Server/src/server_protocol.py
+++ lab3/Pyro/Pyro_Server/src/server_protocol.py
@@ -8,22 +8,28 @@ import utils
 import datetime
 from threading import RLock
 
+# class that holds the locks for each account
+# ensures thread safety
 @Pyro4.behavior(instance_mode="single")
 class MyThreadSafety:
     def __init__(self, id_list) -> None:
         self.locks = {}
         self._initialize_locks(id_list)
-        
+    
+    # return the lock for the corresponding account
     def lock(self, id: int):
         lock = self.locks.get(id)
         if lock is None:
              raise Exception(f"Exception | Lock object is None for ID: {id}")
         return lock
     
+    # insert a lock for the corresponding account
     def put_lock(self, id: int):
         if self.locks.get(id) is None:
             self.locks[id] = RLock()
     
+    # return the locks for 2 account's based on the id
+    # prevent deadlock's
     def two_face_lock(self, idA: int, idB: int):
         if idA < idB:
             return self.lock(idA), self.lock(idB)
@@ -36,7 +42,9 @@ class MyThreadSafety:
 
 
  # Class that holds global locks for clients
+ # needs to be gloabl to be shared between clients
 threadSafety = MyThreadSafety(acc_repo.get_all_accounts())
+@Pyro4.behavior(instance_mode="session")
 @Pyro4.expose
 class ATM_API(object):
     # connect to db
@@ -49,7 +57,8 @@ class ATM_API(object):
     def update_daily_limit(self, id, db_date):
         if utils.day_passed(datetime.date.today(), db_date) >= 1:
             acc_repo.update_daily_limit(self.cursor, id, 900)
-        
+    
+    # get account info
     def info(self, id: int):
         if self.id != id:
             return False, {"message": "Cannot find user, please login correctly."}
@@ -61,12 +70,8 @@ class ATM_API(object):
                 return True, result
             else:
                 return False, {"message": "Cannot find user, please login correctly."}
-        
-            
-    def logout(self, id: int):
-        self.id = -1
-        return True, {"message": "User logged out."}
     
+    # login an account
     def auth(self, id: int, pin: int):  
         ok, result = acc_repo.auth(cursor=self.cursor, id=id, pin=pin)
         if ok:
@@ -77,9 +82,8 @@ class ATM_API(object):
         
         return False, {"message": "Authentication failed, wrong credentials."}
     
-    
+    # register an account
     def create(self, id: int, pin: int, name: str):
-        
         if len(name) == 0:
             return False, {"message": "Name cannot be empty."}
         
@@ -99,7 +103,8 @@ class ATM_API(object):
                 return True, {}
             else:
                 return False, {"message": "Error occurred while creating the account, please try again later."}
-            
+    
+    # deposit money from an  account                 
     def deposit(self, id: int, amount: float):
         with threadSafety.lock(id):
             if amount <= 0:
@@ -118,7 +123,8 @@ class ATM_API(object):
             
             statement_repository.create(self.cursor, Statement(id, "deposit", f"Deposited {amount}", None))
             return True, result
-                
+    
+    # withdraw money from an  account            
     def withdraw(self, id: int,  amount: float):
         if self.id != id:
             return False, {"message": "Bad Request."}
@@ -148,17 +154,8 @@ class ATM_API(object):
             
             statement_repository.create(self.cursor, Statement(id, "withdraw", f"Withdraw {amount}", None))
             return True, result
-        
-    def balance(self, id: int):
-        if self.id != id:
-            return False, {"message": "Bad Request."}
-        
-        with threadSafety.lock(id):
-            ok, result = acc_repo.read(self.cursor, id)
-            if not ok:
-                return False, {"message": "Something went wrong try again, please try again later."}       
-            return True, result
-        
+    
+    # tranfer money between accounts   
     def transfer(self, id_from, id_to: int, name_to: str, amount: float):
         if self.id != id_from:
             return False, {"message": "Bad Request."}
@@ -168,6 +165,7 @@ class ATM_API(object):
         
         lockA, lockB = threadSafety.two_face_lock(id_from, id_to) 
         
+        # lock both clients for the transaction
         with lockA:
             with lockB:
                 if amount <= 0:
@@ -193,7 +191,19 @@ class ATM_API(object):
 
                 statement_repository.create(self.cursor, Statement(id_from, "transfer", f"{amount} transfered from ID: {id_from} to ID: {id_to}", None))
                 return True, result
+    
+    # check the account balance
+    def balance(self, id: int):
+        if self.id != id:
+            return False, {"message": "Bad Request."}
         
+        with threadSafety.lock(id):
+            ok, result = acc_repo.read(self.cursor, id)
+            if not ok:
+                return False, {"message": "Something went wrong try again, please try again later."}       
+            return True, result
+    
+    # get bank statements for an account
     def get_statements(self, id: int):
         if self.id != id:
             return False, {"message": "Bad Request."}
@@ -203,4 +213,6 @@ class ATM_API(object):
         else:
             return False, {"message": "Something went wrong try again, please try again later."}
 
-        
+    def logout(self, id: int):
+        self.id = -1
+        return True, {"message": "User logged out."}
