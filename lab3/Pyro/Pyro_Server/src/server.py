537e6c484b7eff93750e509c341bfaee77669897
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-11-25 04:29:31 +0200

    Pyro: format

--- lab3/Pyro/Pyro_Server/src/server.py
+++ lab3/Pyro/Pyro_Server/src/server.py
@@ -4,15 +4,15 @@ import db
 
 def main():
     db.initialize_db()
-    daemon = Pyro4.Daemon()                # make a Pyro daemon
-    ns = Pyro4.locateNS()                  # find the name server
-    uri = daemon.register(ATM_API)     # register the greeting maker as a Pyro object
+    daemon = Pyro4.Daemon()            # make a Pyro daemon
+    ns = Pyro4.locateNS()              # find the name server
+    uri = daemon.register(ATM_API)     # register the API maker as a Pyro object
     
     ns.register("ATM_API", uri)        # register the object with a name in the name server
 
     print("Server Is Ready To Welcome new Clients.") 
     
-    daemon.requestLoop()                   # start the event loop of the server to wait for calls
+    daemon.requestLoop()               # start the event loop of the server to wait for calls
 
 if __name__ == "__main__":
     main()
