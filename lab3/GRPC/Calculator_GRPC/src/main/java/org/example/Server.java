34db741554d7e2f71445d6cf03103b917c91ca43
DESKTOP-I3LOKU1\leop <leopepp4@gmail.com>
2022-11-27 01:15:32 +0200

    Grpc calculator example

--- lab3/GRPC/Calculator_GRPC/src/main/java/org/example/Server.java
+++ lab3/GRPC/Calculator_GRPC/src/main/java/org/example/Server.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Server {
     public static void main(String[] args) {
-        ServerBuilder<?> serverBuilder = Grpc.newServerBuilderForPort(5050,  InsecureServerCredentials.create());
+        ServerBuilder<?> serverBuilder = Grpc.newServerBuilderForPort(5050, InsecureServerCredentials.create());
         io.grpc.Server server = serverBuilder.addService(new CalculatorServiceImpl()).build();
         try {
             System.out.println("Calculator Server started on port 5050");
