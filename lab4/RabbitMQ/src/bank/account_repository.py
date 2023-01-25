e357ab60638f29bb63a5bff4c6537a44d900263c
DESKTOP-J7GS40K\USER <leopepp4@gmail.com>
2023-01-25 04:10:32 +0200

    rabbitmq docker

---
+++ lab4/RabbitMQ/src/bank/account_repository.py
@@ -0,0 +1,74 @@
+import mariadb
+import datetime
+import db as db
+
+def get_all_accounts():
+    try:
+        conn, cursor = db.db_connection()
+        cursor.execute("SELECT `id` FROM `account` WHERE 1 ")
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        id_list = [int(el["id"]) for el in result]   
+        return id_list
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False, None
+        
+    
+def auth(cursor, id, pin):
+    try:
+        cursor.execute("SELECT `id`, `name`, `date` FROM account WHERE `id`=? AND `pin`=?", [id, pin])
+        result = cursor.fetchone()
+        if result is None:
+            return False, None
+        
+        result["date"] = str(result["date"])
+        return True, result
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False, None
+
+def create(cursor, acc):
+    try:
+        cursor.execute("INSERT INTO `account`(`id`, `pin`, `name`, `balance`, `limit`) VALUES (?,?,?,?,?)", [acc.id, acc.pin, acc.name, 0, acc.limit])
+        return cursor.rowcount >= 1
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False
+
+
+def read(cursor, id):
+    try:
+        cursor.execute("SELECT `id`, `name`, `balance`, `limit`, `date` FROM `account` WHERE `id`=? ",  [id])
+        result = cursor.fetchone()
+        if result is None:
+            return False, None
+        result["date"] = str(result["date"])
+        return True, result
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False, None
+
+def update(cursor, acc):
+    try:
+        cursor.execute("UPDATE account SET `name`=?, `balance`=?, `limit`=? WHERE `id`=?", [acc.name, acc.balance, acc.limit, acc.id])
+        if cursor.rowcount >= 1:
+            return read(cursor=cursor, id=acc.id)
+        else:
+            return False, None
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False, None
+        
+
+def update_daily_limit(cursor, id, limit):
+    try:
+        cursor.execute("UPDATE account SET `limit`=?, `date`=? WHERE `id`=?", [limit, datetime.date.today() ,id])
+        if cursor.rowcount >= 1:
+            return read(cursor=cursor, id=id)
+        else:
+            return False, None
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+        return False, None
---
+++ lab4/RabbitMQ/src/bank/connection_handler.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import pika
+import uuid
+import json
+
+class ConnectionHandler:
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='rabbitmq'))
+
+        self.channel = self.connection.channel()
+
+        self.result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = self.result.method.queue
+
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = json.loads(body)
+
+    def call(self, request):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+        exchange='',
+        routing_key='rpc_queue',
+        properties=pika.BasicProperties(
+            reply_to=self.callback_queue,
+            correlation_id=self.corr_id,
+        ),
+        body=json.dumps(request))
+        self.connection.process_data_events(time_limit=None)
+        return self.response
+
